ea2671ea99dba894b8784449efaeded0c7a37a5e
nguyentanlam25062k <nguyentanlam25062k@gmail.com>
2021-11-07 15:46:25 +0700

    video 25 use ORM to create new data into database

--- backend/src/controllers/homeController.js
+++ backend/src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index'
+import CURDservice from '../services/CRUDservice.js'
 
 let getHomePage = async (req, res) => {
     try {
@@ -9,4 +10,14 @@ let getHomePage = async (req, res) => {
     }
 }
 
-module.exports = { getHomePage }
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CURDservice.createNewUser(req.body)
+    console.log(message)
+    res.send('post crud from server')
+}
+
+module.exports = { getHomePage, getCRUD, postCRUD }
