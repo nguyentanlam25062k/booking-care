1cd7a8d7b59f65f2b6d2918a58e1c459047c94d0
nguyentanlam25062k <nguyentanlam25062k@gmail.com>
2021-11-07 11:59:35 +0700

    video 23 creat all models, migrations, seeders part 1

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import viewEngine from './config/viewEngine'
+import initWebRoutes from './router/web'
+import connectDB from './config/connectDB'
+require('dotenv').config()
+
+let app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+viewEngine(app)
+initWebRoutes(app)
+
+connectDB()
+
+let port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
