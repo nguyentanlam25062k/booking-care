8670da31fd8b88ee4613dddb12bbc1e03fd9a761
nguyentanlam25062k <nguyentanlam25062k@gmail.com>
2021-11-12 12:00:00 +0700

    #46 desgin banner

--- frontend/src/containers/HomePage/HomeHeader.js
+++ frontend/src/containers/HomePage/HomeHeader.js
@@ -4,46 +4,98 @@ import './HomeHeader.scss'
 class HomeHeader extends Component {
     render() {
         return (
-            <div className='home-header-container'>
-                <div className='home-header-content'>
-                    <div className='left-content'>
-                        <i className='fas fa-bars'></i>
-                        <div className='header-logo'></div>
-                    </div>
-                    <div className='center-content'>
-                        <div className='childen-content'>
-                            <div>
-                                <b>Chuyên khoa</b>
+            <>
+                <div className='home-header-container'>
+                    <div className='home-header-content'>
+                        <div className='left-content'>
+                            <i className='fas fa-bars'></i>
+                            <div className='header-logo'></div>
+                        </div>
+                        <div className='center-content'>
+                            <div className='childen-content'>
+                                <div>
+                                    <b>Chuyên khoa</b>
+                                </div>
+                                <div className='sub-title'>Tìm bác sĩ theo chuyên khoa</div>
                             </div>
-                            <div className='sub-title'>Tìm bác sĩ theo chuyên khoa</div>
-                        </div>
-                        <div className='childen-content'>
-                            <div>
-                                <b>Cơ sở y tế</b>
+                            <div className='childen-content'>
+                                <div>
+                                    <b>Cơ sở y tế</b>
+                                </div>
+                                <div className='sub-title'>Chọn bệnh viện phòng khám</div>
                             </div>
-                            <div className='sub-title'>Chọn bệnh viện phòng khám</div>
-                        </div>
-                        <div className='childen-content'>
-                            <div>
-                                <b>Bác sĩ</b>
+                            <div className='childen-content'>
+                                <div>
+                                    <b>Bác sĩ</b>
+                                </div>
+                                <div className='sub-title'>Chọn bác sĩ giỏi</div>
                             </div>
-                            <div className='sub-title'>Chọn bác sĩ giỏi</div>
-                        </div>
-                        <div className='childen-content'>
-                            <div>
-                                <b>Gói khám</b>
+                            <div className='childen-content'>
+                                <div>
+                                    <b>Gói khám</b>
+                                </div>
+                                <div className='sub-title'>Khám sức khỏe tổng quát</div>
                             </div>
-                            <div className='sub-title'>Khám sức khỏe tổng quát</div>
                         </div>
-                    </div>
-                    <div className='right-content'>
-                        <div className='support'>
-                            <i className='fas fa-question-circle'></i>Hỗ trợ
+                        <div className='right-content'>
+                            <div className='support'>
+                                <i className='fas fa-question-circle'></i>Hỗ trợ
+                            </div>
+                            <div className='flag'>VN</div>
                         </div>
-                        <div className='flag'>VN</div>
                     </div>
                 </div>
-            </div>
+                <div className='home-header-banner'>
+                    <div className='content-up'>
+                        <div className='title1'>NỀN TẢNG Y TẾ</div>
+                        <div className='title2'>CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+                        <div className='search'>
+                            <i className='fas fa-search'></i>
+                            <input type='text' placeholder='tìm chuyên khoa khám bệnh' />
+                        </div>
+                    </div>
+                    <div className='content-down'>
+                        <div className='options'>
+                            <div className='options-child'>
+                                <div className='icon-child'>
+                                    <i className='far fa-hospital'></i>
+                                </div>
+                                <div className='text-child'>Khám chuyên khoa</div>
+                            </div>
+                            <div className='options-child'>
+                                <div className='icon-child'>
+                                    <i className='far fa-mobile-alt'></i>
+                                </div>
+                                <div className='text-child'>Khám từ xa</div>
+                            </div>
+                            <div className='options-child'>
+                                <div className='icon-child'>
+                                    <i className='fas fa-procedures'></i>
+                                </div>
+                                <div className='text-child'>Khám tổng quát</div>
+                            </div>
+                            <div className='options-child'>
+                                <div className='icon-child'>
+                                    <i className='fas fa-microscope'></i>
+                                </div>
+                                <div className='text-child'>Xét nghiệm y học</div>
+                            </div>
+                            <div className='options-child'>
+                                <div className='icon-child'>
+                                    <i className='fas fa-user-md'></i>
+                                </div>
+                                <div className='text-child'>Sức khỏe tinh thần</div>
+                            </div>
+                            <div className='options-child'>
+                                <div className='icon-child'>
+                                    <i className='fas fa-tooth'></i>
+                                </div>
+                                <div className='text-child'>Khám nha khoa</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
