527d598569f07ba6706252d5dd291a21e343b567
nguyentanlam25062k <nguyentanlam25062k@gmail.com>
2021-11-13 14:18:08 +0700

    #49 custom react-slicer-carousel

--- frontend/src/containers/HomePage/Section/Specialty.js
+++ frontend/src/containers/HomePage/Section/Specialty.js
@@ -5,40 +5,52 @@ import { FormattedMessage } from 'react-intl'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-
 import './Specialty.scss'
+
 class Specialty extends Component {
     render() {
         let settings = {
             dots: false,
             infinite: true,
             speed: 500,
-            slidesToShow: 1,
+            slidesToShow: 4,
             slidesToScroll: 1
         }
         return (
             <div className='section-specialty'>
-                <div className='specialty-content'>
-                    <Slider {...settings}>
-                        <div className='img-customize'>
-                            <h3>1</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>2</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>3</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>4</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>5</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>6</h3>
-                        </div>
-                    </Slider>
+                <div className='specialty-container'>
+                    <div className='specialty-header'>
+                        <span className='title-section'>Chuyên khoa phổ biến</span>
+                        <button className='btn-section'>Xem thêm</button>
+                    </div>
+                    <div className='specialty-body'>
+                        <Slider {...settings}>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'></div>
+                                <div>Co xuong khop 1</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'></div>
+                                <div>Co xuong khop 2</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'></div>
+                                <div>Co xuong khop 3</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'></div>
+                                <div>Co xuong khop 4</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'></div>
+                                <div>Co xuong khop 5</div>
+                            </div>
+                            <div className='specialty-customize'>
+                                <div className='bg-image'></div>
+                                <div>Co xuong khop 6</div>
+                            </div>
+                        </Slider>
+                    </div>
                 </div>
             </div>
         )
