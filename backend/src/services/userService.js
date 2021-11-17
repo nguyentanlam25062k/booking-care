b115b073a189801f475a84a6284634f7e1f328a4
nguyentanlam25062k <nguyentanlam25062k@gmail.com>
2021-11-17 23:51:14 +0700

    #62 Write and Read file Image with sequelize, mySQL BLOB

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -114,7 +114,8 @@ let createNewUser = (data) => {
                     phoneNumber: data.phoneNumber,
                     gender: data.gender,
                     roleId: data.roleId,
-                    positionId: data.positionId
+                    positionId: data.positionId,
+                    image: data.avatar
                 })
                 resolve({
                     errCode: 0,
@@ -174,6 +175,9 @@ let updateUser = (data) => {
                 user.roleId = data.roleId
                 user.gender = data.gender
                 user.phoneNumber = data.phoneNumber
+                if (data.avatar) {
+                    user.image = data.avatar
+                }
                 await user.save()
                 resolve({
                     errCode: 0,
